0f2e1527be391cc44c03d3db6721d16a401cbc76
paleoduchess <hilary.duke@stonybrook.edu>
2014-02-20 11:34:09 -0500

    overlapping histograms/densities

--- during experiment ggplot plot code.R
+++ during experiment ggplot plot code.R
@@ -24,3 +24,25 @@ p<-ggplot(data, aes(Participant, Strikes))
 
 p + geom_jitter(aes(colour = Participant),cex=3) + scale_y_continuous(name="Strikes", limits=c(0,800), breaks= 0:800*50)
 #would like to include average strikes/participant in this graph
+
+##HISTOGRAMS
+#During experiment data plots
+data<- data.frame(During.experiment.recording_fixed02.11.2014)
+data<-data.frame(data[-(189:190),])
+data$Time.s.=as.numeric(as.character(data$Time.s.))
+Time<-data$Time.s.
+Participant<-data$Participant.number
+Group<-data$Participant.group
+
+
+expert<-subset(data, Group == "Expert") #extract data for experts only
+novice<-subset(data, Group == "Novice") #extract data for novices only
+
+
+##TIME
+
+#plot all participants on x axis and time on y
+
+ggplot(data, aes(Participant.number,Time.s.)) + geom_density(aes(x = Time.s.,
+                                colour = Participant.group)) + labs(x = NULL) +
+  opts(legend.position = "none") + opts(title = "Densities from a kernel density estimator")
