77d27b28126df8ce87186cca2a8040df84e47f65
paleoduchess <hilary.duke@stonybrook.edu>
2014-02-20 10:58:37 -0500

    edits to ggplot code for during experiment

--- during experiment ggplot plot code.R
+++ during experiment ggplot plot code.R
@@ -3,7 +3,24 @@
 data<-During.experiment.recording_fixed02.11.2014
 data<-data[-(189:190),]
 data$Time.s.=as.numeric(as.character(data$Time.s.))
+data$Strikes=as.numeric(as.character(data$Strikes))
+Participant<-data$Participant.number
+Time<-data$Time.s.
+Strikes<-data$Strikes
 
-p<-ggplot(data, aes(factor(Participant.number), Time.s.))
+
+#boxplot for time data across participants
+p<-ggplot(data, aes(Participant, Time))
 p + geom_boxplot()
-p + geom_boxplot(aes(main = "Time (s) for Across Participants", xlab="Participant Number", ylab = "Time (s)")) + geom_jitter(aes(colour = factor(Participant.number)))
+p + geom_boxplot() + geom_jitter(aes(colour = Participant),cex=3)+ scale_y_continuous(name="Time", limits = c(0, 600), breaks = 0:600*50)
+
+
+# strike data across participants
+p<-ggplot(data, aes(Participant, Strikes))
+p + geom_bar(stat="identity")
+p + geom_bar(stat="identity") + geom_jitter(aes(colour = Participant),cex=3) + scale_y_continuous(name="Strikes", limits=c(0,800), breaks= 0:800*50)
+
+p<-ggplot(data, aes(Participant, Strikes))
+
+p + geom_jitter(aes(colour = Participant),cex=3) + scale_y_continuous(name="Strikes", limits=c(0,800), breaks= 0:800*50)
+#would like to include average strikes/participant in this graph
